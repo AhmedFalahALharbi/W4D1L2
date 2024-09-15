22d478c7ae1f51eec0654350250ada0668f990a5
AhmedFalahALharbi <ahmedfalahalharbi@gmail.com>
2024-09-15 13:23:13 +0300

    finish bonus

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-let url = `https://coffee.alexflipnote.dev/random.json`
-
-fetch(url)
-.then(response => response.json())
-.then(data => {
-    let img = document.getElementById('img');
-    img.setAttribute(`src` , data.file)
+function loadImage() {
+    let url = `https://coffee.alexflipnote.dev/random.json`;
+    
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        let img = document.getElementById('img');
+        img.setAttribute('src', data.file);
+    })
+   
 }
-);
+loadImage();
+
+setInterval(loadImage, 2000);
